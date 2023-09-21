c0fe5325af72c1e99e19868895019e7784bc3e00
Akc9912 <sebastianconde60@gmail.com>
2023-09-21 00:37:58 -0300

    Update libreria.h

--- Guia7/libreria.h
+++ Guia7/libreria.h
@@ -14,30 +14,24 @@ typedef struct e_audio
     int garantia;
     float precio;
 } t_audio;
-typedef struct encuesta
-{
-    char nombre[30];
-    int edad;
-    char conoce[3];
-    char consume[3];
-} t_encuesta;
 
-void cargarAudio(t_audio *x)
+void cargarAudio(t_audio *equipo)
 {
     t_audio aux;
-    printf("Ingresar marca del equipo: ");
+    printf("Ingrese los datos del equipo.\n");
+    printf("Marca: ");
     gets(aux.marca);
-    printf("Ingrese modelo del equipo: ");
+    printf("Modelo: ");
     gets(aux.modelo);
-    printf("Ingrese potencia de salida: ");
+    printf("Potencia de salida: ");
     scanf("%d", &aux.pot_salida);
-    printf("Ingrese pais de origen del equipo:");
+    printf("Pais de origen: ");
     scanf("%s", &aux.pais_origen);
-    printf("Ingrese la garantia del equipo (en meses): ");
+    printf("Garantia (meses): ");
     scanf("%d", &aux.garantia);
-    printf("Ingrese el precio del equipo: ");
+    printf("Precio: ");
     scanf("%g", &aux.precio);
-    *x = aux;
+    *equipo = aux;
 }
 void mostrarAudio(t_audio x)
 {
@@ -48,64 +42,29 @@ void mostrarAudio(t_audio x)
     printf("Garantia (en meses): %d\n", x.garantia);
     printf("Precio: %.2f\n", x.precio);
 }
-void mostrarSi(t_audio x)
+void verificaPais(t_audio equipo)
 {
-    int condicion;
-    printf("Marca: %s\n", x.marca);
-    printf("Modelo: %s\n", x.modelo);
-    printf("Potencia de salida: %d\n", x.pot_salida);
-    printf("Pais de origen: %s\n", x.pais_origen);
-    printf("Garantia (en meses): %d\n", x.garantia);
-    condicion = strcmp(x.pais_origen, "Argentina");
-    if ((x.garantia >= 6) && condicion != 0)
+    char arg[20] = {"argentina"};
+    char comparador[20];
+    strcpy(equipo.pais_origen, comparador);
+    if (strcmp(comparador, arg))
     {
-        printf("Precio: %.2f", x.precio);
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
-
-void cargarEncuesta(t_encuesta *x)
+void mostrarSi(t_audio equipo)
 {
-    t_encuesta aux;
-    char S[3] = {"si"}, N[3] = {"no"};
-
-    printf("Ingrese el nombre: ");
-    gets(x.nombre);
-    printf("Ingrese la edad de %s: ", x.nombre);
-    scanf("%d", &x.edad);
-    printf("Conoce el producto XXX?: ");
-    gets(x.conoce);
-
-    if (strcmp(s, x.conoce) != 0)
+    printf("Marca: %s\n", equipo.marca);
+    printf("Modelo: %s\n", equipo.modelo);
+    printf("Potencia de salida: %d\n", equipo.pot_salida);
+    printf("Pais de origen: %s\n", equipo.pais_origen);
+    printf("Garantia (en meses): %d\n", equipo.garantia);
+    if (equipo.garantia >= 6 && verificaPais(equipo))
     {
-        printf("Consume el producto XXX)");
-        gets(x.consume);
+        printf("Precio: %.2f", equipo.precio);
     }
-    *x = aux;
-}
-
-void cargarVecEncuestas(t_encuesta vec[], int n)
-{
-    int i;
-    t_encuesta encuestado;
-    for (i = 0; i < n; i++)
-    {
-        vec[i] = cargarEncuesta(encuestado);
-    }
-}
-float porcentajeConoce(t_encuesta vec[], , int n)
-{
-    float porcentaje;
-    t_encuesta x;
-    int i, contador = 0;
-    char S[3] = {"si"};
-    for (i = 0; i < n; i++)
-    {
-        x = v[i];
-        if (strcmp(s, x.conoce))
-        {
-            contador++;
-        }
-    }
-    porcentaje = (n * 100) / contador;
-    return porcentaje;
 }
